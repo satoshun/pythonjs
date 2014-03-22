8abca8c4754add6dc434a053151b2548c17fbf91
SatoShun <shun.sato1@gmail.com>
2014-03-22 19:51:46 +0900

    build version 0.0.4

--- pythonjs.js
+++ pythonjs.js
@@ -35,6 +35,9 @@
     },
     extend: function extend(target) {
       this.push.apply(this, target);
+    },
+    copy: function copy() {
+      return _.clone(this);
     }
   });
 } (this));
@@ -121,20 +124,20 @@
     undef = void 0;
 
   _.extend(root.String.prototype, {
-    count: function count (key) {
+    count: function count(key) {
       if (key === undef) return 0;
       return (this.match(new RegExp(key, 'g')) || []).length;
     },
-    title: function title () {
+    title: function title() {
       var s = String(this);
       return s.replace(/[a-zA-Z]\S*/g, function (word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
       });
     },
-    capitalize: function capitalize () {
+    capitalize: function capitalize() {
       return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
     },
-    lstrip: function lstrip (chars) {
+    lstrip: function lstrip(chars) {
       var start = 0,
           length = this.length;
       if (chars === undef) {
@@ -144,7 +147,7 @@
       }
       return this.slice(start);
     },
-    rstrip: function rstrip (chars) {
+    rstrip: function rstrip(chars) {
       var length = this.length,
           end = length - 1;
       if (chars === undef) {
@@ -154,10 +157,10 @@
       }
       return this.slice(0, end + 1);
     },
-    strip: function strip (chars) {
+    strip: function strip(chars) {
       return this.lstrip(chars).rstrip(chars);
     },
-    center: function center (length, char) {
+    center: function center(length, char) {
       var r, l;
       if (this.length >= length) return this;
       if (char === undef) char = ' ';
@@ -167,47 +170,47 @@
       else l += (length % 2);
       return repeatString(char, r) + this + repeatString(char, l);
     },
-    endswith: function endswith (chars) {
+    endswith: function endswith(chars) {
       return this.slice(this.length - chars.length) === chars;
     },
-    startswith: function startswith (chars) {
+    startswith: function startswith(chars) {
       return this.slice(0, chars.length) === chars;
     },
-    expandtabs: function expandtabs (length) {
+    expandtabs: function expandtabs(length) {
       var s = String(this);
       return s.replace(/\s/g, function (tab) {
         return repeatString(' ', length);
       });
     },
-    isalnum: function isalnum () {
+    isalnum: function isalnum() {
       return (this.match(/^\w*$/) || [''])[0] === String(this);
     },
-    isalpha: function isalpha () {
+    isalpha: function isalpha() {
       return (this.match(/^[a-zA-Z]*$/) || [''])[0] === String(this);
     },
-    isdigit: function isdigit () {
+    isdigit: function isdigit() {
       return (this.match(/^\d*$/) || [''])[0] === String(this);
     },
-    islower: function islower () {
+    islower: function islower() {
       return (this.match(/^[a-z]*$/) || [''])[0] === String(this);
     },
-    isupper: function isupper () {
+    isupper: function isupper() {
       return (this.match(/^[A-Z]*$/) || [''])[0] === String(this);
     },
-    isspace: function isspace () {
+    isspace: function isspace() {
       return (this.match(/^\s*$/) || [''])[0] === String(this);
     },
-    rjust: function rjust (length, char) {
+    rjust: function rjust(length, char) {
       if (this.length >= length) return this;
       if (char === undef) char = ' ';
       return repeatString(char, length - this.length) + this;
     },
-    ljust: function ljust (length, char) {
+    ljust: function ljust(length, char) {
       if (this.length >= length) return this;
       if (char === undef) char = ' ';
       return this + repeatString(char, length - this.length);
     },
-    rsplit: function rsplit (char, num) {
+    rsplit: function rsplit(char, num) {
       var words, start = 0, end;
       if (char === undef) char = '\n';
       words = this.split(char);
@@ -220,7 +223,7 @@
       }
       return words.slice(start, end);
     },
-    lsplit: function lsplit (char, num) {
+    lsplit: function lsplit(char, num) {
       var words, start = 0, end, length;
       if (char === undef) char = '\n';
       words = this.split(char);
@@ -233,7 +236,7 @@
       }
       return words.slice(start, end);
     },
-    splitlines: function splitlines (keepend) {
+    splitlines: function splitlines(keepend) {
       var words = this.split('\n');
       if (keepend) {
         for (var i = 0, l = words.length - 1; i < l; i++) {
@@ -243,7 +246,7 @@
 
       return words[words.length - 1] === '' || words[words.length - 1] === '\n' ? words.slice(0, words.length - 1) : words;
     },
-    format: function format () {
+    format: function format() {
       var s = String(this),
           values = Array.prototype.slice.call(arguments),
           value, length = values.length, i = 0, index = 0,
@@ -266,7 +269,7 @@
 
       return s;
     },
-    zfill: function zfill (length) {
+    zfill: function zfill(length) {
       return this.rjust(length, '0');
     }
   });
