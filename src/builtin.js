e59a044c31785fff65f5f0cf4e9f1248ceb645d2
SatoShun <shun.sato1@gmail.com>
2014-08-03 05:45:13 +0000

    add builtin: all method

--- src/builtin.js
+++ src/builtin.js
@@ -3,6 +3,13 @@
 
   root.py = root.py || {};
   _.extend(root.py, {
-    abs: Math.abs
+    abs: Math.abs,
+    all: function all(iterable) {
+      var i = 0, len = iterable.length;
+      for (; i < len; i++) {
+        if (!iterable[i]) return false;
+      }
+      return true;
+    }
   });
 }(this));
--- test/builtin.spec.js
+++ test/builtin.spec.js
@@ -6,4 +6,13 @@ describe('global builtin test', function() {
     expect(10.1).toEqual(py.abs(-10.1));
     expect(10.1).toEqual(py.abs(10.1));
   });
+
+  it('all', function() {
+    expect(true).toEqual(py.all([1,4, -1]));
+    expect(true).toEqual(py.all([true, -1, 1]));
+    expect(true).toEqual(py.all(['0', 'false']));
+
+    expect(false).toEqual(py.all([1,4, 0]));
+    expect(false).toEqual(py.all([false, true, true, false]));
+  });
 });
